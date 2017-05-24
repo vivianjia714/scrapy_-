9a8583e1075048cf1100deb013909c93cc2b1afe
vivianjia714 <271626310@qq.com>
2017-05-24 04:51:11 +0100

    aaa

---
+++ offer_51/pipelines.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import MySQLdb
+
+class Offer51Pipeline(object):
+    def __init__(self):
+        self.conn = MySQLdb.connect(host="localhost",user="root",passwd="123456",db="51db",charset="utf8")
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+#        sql = """insert ignore into t_uk(title, school_url, IELTS_grade, hotmajor,Timerank,QSrank) values(%s, %s, %s, %s,%s,%s)"""
+        sql = """insert ignore into t_sg(title, school_url, IELTS_grade, hotmajor,Timerank,QSrank) values(%s, %s, %s, %s,%s,%s)"""
+        param = (item['title'], item['school_url'], item['IELTS_grade'], item['hotmajor'],item['Timerank'],item['QSrank'])
+        self.cursor.execute(sql,param)
+        self.conn.commit()
+
+
+#    def process_item(self, item, spider):
+#        pass
+    
+
+    
+
+
+    
+
+    
